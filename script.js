3833903fddf3acc2fd71e33604961c6619104f6c
ksn38 <ksn38@yandex.ru>
2020-10-24 21:07:21 +0300

    -

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let x = document.querySelector('.out');
 let button = document.querySelector('.button');
 let text = document.querySelector('p');
 let y = 1;
-let day = new Map();
+
 let z = 0;
 
 
 let dict = function (dif) {
+  let day = new Map();
   let dateLast = new Date();
   dateLast.setDate(dateLast.getDate() - dif);
   dateLast = dateLast.toISOString().slice(0, 10);
@@ -31,16 +32,24 @@ button.onclick = function () {
   y += 1;
   x.textContent = y;
   let dict1 = new Map([...dict(1).entries()]);
-  let dict2 = new Map([...dict(2).entries()]);
+  let dict2 = new Map([...dict(8).entries()]);
   let listKeys = [...dict1.keys()];
   let outMap = new Map();
-  console.log(listKeys[0]);
   for (let key = 0; key < listKeys.length; key++) {
-    outMap.set(listKeys[key], (((dict1.get(listKeys[key])/dict2.get(listKeys[key])) - 1) * 100));
+    let val = ((dict1.get(listKeys[key])/dict2.get(listKeys[key])) - 1) * 10000;
+    if (!Number.isNaN(val)) {
+      val = Math.round(val, 2);
+      val = val/100;
+      outMap.set(listKeys[key], val);
+    };
   };
+  outMaprev = new Map([...outMap.entries()].sort((a,b) => a[1] - b[1]));
   outMap = new Map([...outMap.entries()].sort((a,b) => b[1] - a[1]));
   console.log(outMap);
+  console.log([...outMap].slice(0, 20));
+  console.log([...outMaprev].slice(0, 20));
 };
+
 
 /*let dict = function (dif) {
   let day = new Map();
