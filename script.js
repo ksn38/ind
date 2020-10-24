7ba2f742698f4ed3d6cd738a2311c8138de202af
ksn38 <ksn38@yandex.ru>
2020-10-24 22:04:23 +0300

    -

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let x = document.querySelector('.out');
+let namesHigh = document.querySelectorAll('.name-high');
+let namesLow = document.querySelectorAll('.name-low');
+let changesHigh = document.querySelectorAll('.change-high');
+let changesLow = document.querySelectorAll('.change-low');
 let button = document.querySelector('.button');
 let text = document.querySelector('p');
 let y = 1;
-
-let z = 0;
 
 
 let dict = function (dif) {
@@ -30,9 +31,9 @@ let dict = function (dif) {
 
 button.onclick = function () {
   y += 1;
-  x.textContent = y;
+  console.log(y);
   let dict1 = new Map([...dict(1).entries()]);
-  let dict2 = new Map([...dict(8).entries()]);
+  let dict2 = new Map([...dict(36).entries()]);
   let listKeys = [...dict1.keys()];
   let outMap = new Map();
   for (let key = 0; key < listKeys.length; key++) {
@@ -43,27 +44,17 @@ button.onclick = function () {
       outMap.set(listKeys[key], val);
     };
   };
-  outMaprev = new Map([...outMap.entries()].sort((a,b) => a[1] - b[1]));
+  outMapRev = new Map([...outMap.entries()].sort((a,b) => a[1] - b[1]));
   outMap = new Map([...outMap.entries()].sort((a,b) => b[1] - a[1]));
-  console.log(outMap);
-  console.log([...outMap].slice(0, 20));
-  console.log([...outMaprev].slice(0, 20));
+  outMapKeys = [...outMap.keys()].slice(0, 20);
+  outMapValues = [...outMap.values()].slice(0, 20);
+  outMapRevKeys = [...outMapRev.keys()].slice(0, 20);
+  outMapRevValues = [...outMapRev.values()].slice(0, 20);
+  for (let i = 0; i < namesHigh.length; i++) {
+    namesHigh[i].textContent = outMapKeys[i];
+    changesHigh[i].textContent = outMapValues[i];
+    namesLow[i].textContent = outMapRevKeys[i];
+    changesLow[i].textContent = outMapRevValues[i];
+  };
 };
 
-
-/*let dict = function (dif) {
-  let day = new Map();
-  let dateLast = new Date();
-  dateLast.setDate(dateLast.getDate() - dif);
-  dateLast = dateLast.toISOString().slice(0, 10);
-  for (let i = 0; i < 201; i += 100) {
-    let url = 'http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.json?date=' + dateLast + '&start=' + i;
-    $.getJSON(url, function(data) {
-      let rows = data.history.data;
-      for (let row = 0; row < rows.length; row++) {
-        day.set(rows[row][2], rows[row][9]);
-      };      
-    });
-  };
-  return day;
-};*/
